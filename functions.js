cd88e996e858cbc39e17124af81a8305f05a9941
danapham <danapham12@gmail.com>
2020-07-20 19:57:10 -0500

    created some more functions

---
+++ functions.js
@@ -0,0 +1,28 @@
+// console.log("functions")
+
+// const nuggetizer = (animal) => {
+//   return `processed ${animal}`;
+// }
+
+// console.log(nuggetizer('pig'));
+// console.log(nuggetizer('fish'));
+// console.log(nuggetizer('chicken'));
+// console.log(nuggetizer('student'));
+
+// const addFourtyTwo = (number) => {
+//   return number + 42;
+// }
+
+// console.log(addFourtyTwo(1));
+
+const age2099 = (born) => {
+  return 2099 - born;
+}
+
+console.log(age2099(2049));
+
+const otherWay = (born) => {
+  return `If you're still alive, you will be ${2099 - born} in 2099.`
+}
+
+console.log(otherWay(1994));
